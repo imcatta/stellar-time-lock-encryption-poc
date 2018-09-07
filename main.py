514b649d11d5b874b57894b7348564a4dbba6036
Andrea Cattaneo <andreacattaneo96@gmail.com>
2018-09-07 22:08:28 +0200

    Added delta parameter

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def initialize_keypair(add_funds=True):
 PRIZE = '100'
 PAWN = '100'
 COUNTERPRIZE = '1'
+DELTA = 60  # seconds
 
 x = 'Hello, World!'  # Carol's message
 x1, x2 = PlaintextToHexSecretSharer.split_secret(x, 2, 2)
@@ -60,10 +61,13 @@ address_gamma.get()
 starting_sequence_rho = int(address_rho.sequence)
 starting_sequence_gamma = int(address_gamma.sequence)
 
-T = datetime.datetime.now() + datetime.timedelta(seconds=30)
-T_unix = int(time.mktime(T.timetuple()))
-timebound_transazione = TimeBounds(minTime=T_unix, maxTime=0)
-timebound_controtransazione = TimeBounds(minTime=0, maxTime=T_unix)
+tau = datetime.datetime.now() + datetime.timedelta(seconds=30)
+tau_unix = int(time.mktime(tau.timetuple()))
+tau_plus_delta = tau + datetime.timedelta(seconds=DELTA)
+tau_plus_delta_unix = int(time.mktime(tau_plus_delta.timetuple()))
+timebound_transazione = TimeBounds(
+    minTime=tau_unix, maxTime=tau_plus_delta_unix)
+timebound_controtransazione = TimeBounds(minTime=0, maxTime=tau_unix)
 
 builder_rho_1 = Builder(secret=kp_rho_seed,
                         sequence=starting_sequence_rho+1)
@@ -132,7 +136,7 @@ response = builder.submit()
 assert 'hash' not in response
 
 print('Alice tries to submit rho_2 before the deadline, but fails')
-print(f'[deadline: { T }; current time: { datetime.datetime.now() }]')
+print(f'[deadline: { tau }; current time: { datetime.datetime.now() }]')
 envelope = Te(tx_rho_2, opts={})
 response = horizon.submit(envelope.xdr())
 assert 'hash' not in response
@@ -144,7 +148,7 @@ response = horizon.submit(envelope.xdr())
 assert 'hash' in response
 
 print('Waiting for the deadline')
-tts = (T - datetime.datetime.now()).total_seconds()
+tts = (tau - datetime.datetime.now()).total_seconds()
 time.sleep(tts)
 time.sleep(5)  # some margin
 
